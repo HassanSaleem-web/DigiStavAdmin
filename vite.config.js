7a931f744500052bc5c356992cdecfd213ffbd40
Hassan Saleem <smuhammadhassan39@gmail.com>
2025-11-26 13:26:21 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    
   ],
+  preview: {
+    host: "0.0.0.0",
+    port: 10000,
+    allowedHosts: ["digistavadmin.onrender.com"] // ← IMPORTANT
+  },
+  server: {
+    host: "0.0.0.0"
+  }
 })
